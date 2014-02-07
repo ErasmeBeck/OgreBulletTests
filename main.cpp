381385ac5c6cef2fcd2924bc7b86fe6cd3ec4c82
Erasme <erasmebeck@gmail.com>
2014-02-07 08:10:45 +0100

    Test de commit: résolution du problème de mdp

--- main.cpp
+++ main.cpp
@@ -33,8 +33,6 @@ public:
         btQuaternion rot = worldTrans.getRotation();
         mVisibleobj->setOrientation(rot.w(), rot.x(), rot.y(), rot.z());
         btVector3 pos = worldTrans.getOrigin();
-        // TODO **** XXX need to fix this up such that it renders properly since this doesnt know the scale of the node
-        // also the getCube function returns a cube that isnt centered on Z
         mVisibleobj->setPosition(pos.x(), pos.y()+5, pos.z()-5);
     }
 protected:
