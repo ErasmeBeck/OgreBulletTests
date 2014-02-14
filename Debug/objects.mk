52b2814a5a8ede660d9a78815b1820a7d63f2829
Erasme <erasmebeck@gmail.com>
2014-02-14 10:43:29 +0100

    le fichier resources.cfg n'était pas le bon, correction faite

--- Debug/objects.mk
+++
@@ -1,8 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-################################################################################
-
-USER_OBJS :=
-
-LIBS := -lOgreMain -lOIS -lboost_system -lOgreOverlay -lOgreTerrain -lSDL -lGLU -lGL -lBulletSoftBody -lBulletDynamics -lBulletCollision -lLinearMath
-
